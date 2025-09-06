ff138b15a79cd0bf94cd24bc23e7e3d1e6088193
greemaltend4 <dengestabiy@hotmail.com>
2025-09-06 13:55:52 -0500

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.json());
+
+// Connexion à MongoDB
+mongoose.connect('mongodb://localhost:27017/ecommerce', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connecté à MongoDB...'))
+  .catch(err => console.error('Erreur de connexion à MongoDB:', err));
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Bienvenue sur l API d intégration cloud e-commerce!');
+});
+
+app.listen(PORT, () => {
+  console.log(
+    `Serveur en marche sur http://localhost:${PORT}`
+  );
+});
